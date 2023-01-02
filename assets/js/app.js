277880eb33952c915556a9ae76b195a8a89efbd2
Sohail <mr.sohailsarwar@gmail.com>
2023-01-02 16:39:00 +0400

    Add hero slide (underdevelopment)

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,3 +48,35 @@ function bookmarkData(obj) {
 }
 
 myBookmarks();
+
+
+
+document.querySelectorAll('.slide_wrap').forEach(item => {
+    let slider = item;
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.style.cursor = 'grabbing';
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.style.cursor = 'grab';
+    });
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.style.cursor = 'grab';
+    });
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 3; //scroll-fast
+        slider.scrollLeft = scrollLeft - walk;
+        //console.log(walk);
+    });
+});
