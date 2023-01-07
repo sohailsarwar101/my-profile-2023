2a312c877d98fc4daa6e34c53c66bf2d2d8421ee
Sohail <mr.sohailsarwar@gmail.com>
2023-01-07 17:12:44 +0400

    Fix slide for mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -51,34 +51,63 @@ myBookmarks();
 
 
 
-document.querySelectorAll('.slider_inner').forEach(item => {
-    let slider = item;
-    let isDown = false;
-    let startX;
-    let scrollLeft;
+// document.querySelectorAll('.slider_inner').forEach(item => {
+//     let slider = item;
+//     let isDown = false;
+//     let startX;
+//     let scrollLeft;
 
-    slider.addEventListener('mousedown', (e) => {
-        isDown = true;
-        slider.style.cursor = 'grabbing';
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-        slider.style.cursor = 'grab';
-    });
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-        slider.style.cursor = 'grab';
-    });
-    slider.addEventListener('mousemove', (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = (x - startX) * 3; //scroll-fast
-        slider.scrollLeft = scrollLeft - walk;
-        //console.log(walk);
-    });
+//     slider.addEventListener('mousedown', (e) => {
+//         isDown = true;
+//         slider.style.cursor = 'grabbing';
+//         startX = e.pageX - slider.offsetLeft;
+//         scrollLeft = slider.scrollLeft;
+//     });
+//     slider.addEventListener('mouseleave', () => {
+//         isDown = false;
+//         slider.style.cursor = 'grab';
+//     });
+//     slider.addEventListener('mouseup', () => {
+//         isDown = false;
+//         slider.style.cursor = 'grab';
+//     });
+//     slider.addEventListener('mousemove', (e) => {
+//         if (!isDown) return;
+//         e.preventDefault();
+//         const x = e.pageX - slider.offsetLeft;
+//         const walk = (x - startX) * 3; //scroll-fast
+//         slider.scrollLeft = scrollLeft - walk;
+//         //console.log(walk);
+//     });
+// });
+
+
+const slider = document.querySelector('.slider_inner');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
 });
 
 
